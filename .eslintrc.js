04fab46dc666c751883aadd8ac2d22242741c79f
Felipe Campara <fcamparasilva@gmail.com>
2019-09-11 15:17:45 -0400

    Init commit

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: [
+    'airbnb',
+    'prettier',
+    'prettier/react'
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+    'react/jsx-filename-extension': [
+      'warn',
+      { extensions: ['.jsx', '.js']}
+    ],
+    'semi': ['error', 'never'],
+    'import/prefer-default-export': 'off'
+  },
+};
